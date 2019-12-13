1454d520cdad0f39009e46be41464fee80815728
Alex Tarasiuk <antar37@gmail.com>
2019-12-13 10:16:53 -0600

    Added SchoolMap

---
+++ schoolMap.js
@@ -0,0 +1,269 @@
+let schools = [
+                {
+                    "type": "point",
+                    "lat": 29.633576,
+                    "lon": -98.602698,
+                    "entity_id": "8590",
+                    "popup": "",
+                    "alt": "school",
+                    "icon": {
+                        "iconUrl": "/themes/custom/eam/images/map-icon-school.svg",
+                        "iconSize": {
+                            "x": 33,
+                            "y": 51
+                        },
+                        "iconAnchor": {
+                            "x": 16,
+                            "y": 51
+                        },
+                        "className": "nid--8590"
+                    }
+                },
+                {
+                    "type": "point",
+                    "lat": 28.403445,
+                    "lon": -97.745142,
+                    "entity_id": "8887",
+                    "popup": "",
+                    "alt": "school",
+                    "icon": {
+                        "iconUrl": "/themes/custom/eam/images/map-icon-school.svg",
+                        "iconSize": {
+                            "x": 33,
+                            "y": 51
+                        },
+                        "iconAnchor": {
+                            "x": 16,
+                            "y": 51
+                        },
+                        "className": "nid--8887"
+                    }
+                },
+                {
+                    "type": "point",
+                    "lat": 25.927991,
+                    "lon": -97.508808,
+                    "entity_id": "8888",
+                    "popup": "",
+                    "alt": "school",
+                    "icon": {
+                        "iconUrl": "/themes/custom/eam/images/map-icon-school.svg",
+                        "iconSize": {
+                            "x": 33,
+                            "y": 51
+                        },
+                        "iconAnchor": {
+                            "x": 16,
+                            "y": 51
+                        },
+                        "className": "nid--8888"
+                    }
+                },
+                {
+                    "type": "point",
+                    "lat": 27.786451,
+                    "lon": -97.397447,
+                    "entity_id": "8889",
+                    "popup": "",
+                    "alt": "school",
+                    "icon": {
+                        "iconUrl": "/themes/custom/eam/images/map-icon-school.svg",
+                        "iconSize": {
+                            "x": 33,
+                            "y": 51
+                        },
+                        "iconAnchor": {
+                            "x": 16,
+                            "y": 51
+                        },
+                        "className": "nid--8889"
+                    }
+                },
+                {
+                    "type": "point",
+                    "lat": 26.202276,
+                    "lon": -97.679259,
+                    "entity_id": "8896",
+                    "popup": "",
+                    "alt": "school",
+                    "icon": {
+                        "iconUrl": "/themes/custom/eam/images/map-icon-school.svg",
+                        "iconSize": {
+                            "x": 33,
+                            "y": 51
+                        },
+                        "iconAnchor": {
+                            "x": 16,
+                            "y": 51
+                        },
+                        "className": "nid--8896"
+                    }
+                },
+                {
+                    "type": "point",
+                    "lat": 29.481685,
+                    "lon": -98.438549,
+                    "entity_id": "8901",
+                    "popup": "",
+                    "alt": "school",
+                    "icon": {
+                        "iconUrl": "/themes/custom/eam/images/map-icon-school.svg",
+                        "iconSize": {
+                            "x": 33,
+                            "y": 51
+                        },
+                        "iconAnchor": {
+                            "x": 16,
+                            "y": 51
+                        },
+                        "className": "nid--8901"
+                    }
+                },
+                {
+                    "type": "point",
+                    "lat": 29.530961,
+                    "lon": -98.524513,
+                    "entity_id": "8902",
+                    "popup": "",
+                    "alt": "school",
+                    "icon": {
+                        "iconUrl": "/themes/custom/eam/images/map-icon-school.svg",
+                        "iconSize": {
+                            "x": 33,
+                            "y": 51
+                        },
+                        "iconAnchor": {
+                            "x": 16,
+                            "y": 51
+                        },
+                        "className": "nid--8902"
+                    }
+                },
+                {
+                    "type": "point",
+                    "lat": 29.485753,
+                    "lon": -98.47894,
+                    "entity_id": "8903",
+                    "popup": "",
+                    "alt": "school",
+                    "icon": {
+                        "iconUrl": "/themes/custom/eam/images/map-icon-school.svg",
+                        "iconSize": {
+                            "x": 33,
+                            "y": 51
+                        },
+                        "iconAnchor": {
+                            "x": 16,
+                            "y": 51
+                        },
+                        "className": "nid--8903"
+                    }
+                },
+                {
+                    "type": "point",
+                    "lat": 29.583364,
+                    "lon": -98.384736,
+                    "entity_id": "8904",
+                    "popup": "",
+                    "alt": "school",
+                    "icon": {
+                        "iconUrl": "/themes/custom/eam/images/map-icon-school.svg",
+                        "iconSize": {
+                            "x": 33,
+                            "y": 51
+                        },
+                        "iconAnchor": {
+                            "x": 16,
+                            "y": 51
+                        },
+                        "className": "nid--8904"
+                    }
+                },
+                {
+                    "type": "point",
+                    "lat": 29.905397,
+                    "lon": -97.995855,
+                    "entity_id": "8905",
+                    "popup": "",
+                    "alt": "school",
+                    "icon": {
+                        "iconUrl": "/themes/custom/eam/images/map-icon-school.svg",
+                        "iconSize": {
+                            "x": 33,
+                            "y": 51
+                        },
+                        "iconAnchor": {
+                            "x": 16,
+                            "y": 51
+                        },
+                        "className": "nid--8905"
+                    }
+                },
+                {
+                    "type": "point",
+                    "lat": 29.213366,
+                    "lon": -99.787754,
+                    "entity_id": "8907",
+                    "popup": "",
+                    "alt": "school",
+                    "icon": {
+                        "iconUrl": "/themes/custom/eam/images/map-icon-school.svg",
+                        "iconSize": {
+                            "x": 33,
+                            "y": 51
+                        },
+                        "iconAnchor": {
+                            "x": 16,
+                            "y": 51
+                        },
+                        "className": "nid--8907"
+                    }
+                },
+                {
+                    "type": "point",
+                    "lat": 30.032541,
+                    "lon": -98.038908,
+                    "entity_id": "8908",
+                    "popup": "",
+                    "alt": "school",
+                    "icon": {
+                        "iconUrl": "/themes/custom/eam/images/map-icon-school.svg",
+                        "iconSize": {
+                            "x": 33,
+                            "y": 51
+                        },
+                        "iconAnchor": {
+                            "x": 16,
+                            "y": 51
+                        },
+                        "className": "nid--8908"
+                    }
+                }
+            ]
+
+var map = L.map('mapid', {
+   center: [29.424122, -98.493629],
+   zoom: 8,
+   scrollWheelZoom:false,
+});
+
+var SchoolIcon = L.Icon.extend({
+   options: {
+      iconSize:     [38, 60],
+      iconAnchor:   [22, 59],
+      popupAnchor:  [-3, -76]
+   }
+});
+
+var redIcon = new SchoolIcon({iconUrl: 'https://uploads-ssl.webflow.com/5d9b72537a2a6e593a84089e/5d9f32a6a121e084af9962b9_map-icon-church.svg'});
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+   id: 'mapbox.streets',
+   accessToken: 'pk.eyJ1Ijoicmtkc3R1ZGlvIiwiYSI6ImNrMWpzaHVhNjIzeXgzYnA2aDd3bW92YzQifQ._P1ZMrxr85X9J8twAPYy-A'
+}).addTo(map);
+
+var markers = L.markerClusterGroup();
+schools.forEach(function(school){
+   markers.addLayer(L.marker([school.lat, school.lon], {icon: redIcon}));
+});
+map.addLayer(markers);
